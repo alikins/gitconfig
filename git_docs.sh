7e338710d473530f34c1f095deb9e9b7390b0bd2
Adrian Likins <alikins@redhat.com>
2013-02-06 16:53:02 -0500

    add some notes about what git_docs.sh does

--- git_docs.sh
+++ git_docs.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
+#
+# This attempts to generate a klugey API history report of python
+# modules. It does this my creating a revision of the docs for
+# every tagged version of the source. With a simple text based
+# docs, end result is a 'git log -p' in the docs history repo
+# should give you readable api diff between versions.
+#
+# Don't expect this to work without hacking.
+#
+# Checkout out every tag of a git repo of a python project, 
+#  generate epydoc for it, and commit to git repo with tags
+#
+#
+#
+# git_docs.sh /git/repo/with/python/code /git/repo/to/check/docs/into /src /test
 
+# path to a git repo to checkout revisions from
+# currently it will checkout each tag
+SRCDIR=$1
+shift
 
-DOCS_DIR=/home/adrian/src/subscription-manager-docs/
+# where to write the docs to
+DOCS_DIR=$1
+shift
+
+# subdirs where to find *.py where to find the code we are generating docs from
+PYDIRS="$*"
 
 gen_docs() {
-    for py in $(git ls-files src/subscription_manager/ src/rct/ src/plugins/ src/subscription_manager/gui/| grep .py$);
+    for py in $(git ls-files ${PYDIRS} | grep .py$);
     do
         echo "$py"
 
-        DOC=$(epydoc --debug --text "$py");
+        DOC_EXCLUDES="object.__"
+        # greps and sed's just to clean up some noise in the output
+        DOC=$(epydoc --simple-term --parse-only --debug --text "$py" | \
+            grep -v "${DOC_EXCLUDES}" | \
+            sed 's/[[:space:]]\+$//' | \
+            sed 's/|/ /' | \
+            sed 's/^[[:space:]]*$//');
         #echo "$DOC"
         MODULENAME=$(echo "$DOC" |sed -n '/^MODULE NAME/{ N;; s/[ \t]*.*\n//p; }' | tr -d ' ');
         if [ -n "${MODULENAME}" ] ; then
@@ -17,9 +47,11 @@ gen_docs() {
         done;
 }
 
-
-SRCDIR="/home/adrian/src/subscription-manager"
-for tag in $(cat tags.txt);
+SRCDIR="/home/adrian/src/doctest/subscription-manager"
+#for tag in $(cat tags.txt);
+TAGS=$(git tag -l | sort --version-sort | grep -v "^-")
+TAGS="$TAGS master"
+for tag in $TAGS
 do
     echo "$tag"; 
     git checkout "$tag";
